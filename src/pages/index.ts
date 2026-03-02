d75ccc06442905bb8fc87542781729087cf0b93c
lain <hlolkayla038@gmail.com>
2026-03-01 21:27:26 -0600

    Delete src/pages directory

--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(500).json({
-    status: 500,
-    message: "I don't really know what to do with this URL anymore"
-  });
-}
